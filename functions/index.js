bef7e73fd8437be457312b8b1a325b30dda60813
Ivan Villa <ivan@wearescribe.com>
2018-05-29 22:19:16 -0700

    endpoint for sms created
    created an sms endpoint for twilio to send messages too when a user sends a text

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 const express = require('express');
+var bodyParser = require('body-parser');
+const twilio = require('twilio');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const CONFIG = require('./config.js');
 
 
 /*******************
@@ -8,10 +12,27 @@ const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 // routes ---
-app.get('/', (request, response)=> {
+app.get('/api', (request, response) => {
   response.send("Welcome to the API");
 });
+
+//takes in any reply message
+app.post('/api/sms', (request, response) => {
+  const twiml = new MessagingResponse();
+  const body = request.body.Body;
+
+  twiml.message(`you just sent: ${body} to simple reservation app`);
+
+  response.writeHead(200, {'Content-Type': 'text/xml'});
+  response.end(twiml.toString());
+});
+
+
 
 exports.api = functions.https.onRequest(app);
 
@@ -20,7 +41,14 @@ exports.api = functions.https.onRequest(app);
 * Other Functions
 ********************/
 
-// exports.myFunctionName = functions.firestore
-//     .document('users/marie').onWrite((change, context) => {
-//       /* ... Your code here */
+// exports.smstest = functions.firestore.document('Reservation').onWrite((change, context) => {
+//   var client = new twilio(CONFIG.sid, CONFIG.token);
+
+//   client.messages.create({
+//     body: 'Hello from Node',
+//     to: 'number',  // Text this number
+//     from: 'number' // From a valid Twilio number
+//   })
+//     .then((message) => console.log(message.sid));
+
 // });
