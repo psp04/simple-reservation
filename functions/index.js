b4562bc7760b52521a10287cc073ee51731c26f7
Ivan Villa <ivan@wearescribe.com>
2018-06-06 20:38:19 -0700

    send-recieve-text
    The app now responds to texts and sends text on database update

--- functions/index.js
+++ functions/index.js
@@ -1,54 +1,48 @@
+const CONFIG = require('./config.js');
 const functions = require('firebase-functions');
 const express = require('express');
-var bodyParser = require('body-parser');
-const twilio = require('twilio');
+const bodyParser = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const CONFIG = require('./config.js');
 
-
-/*******************
-* REST API
-********************/
-
+/**********************
+ * HTTP Trigger
+ ***********************/
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// routes ---
+// root api ---
 app.get('/api', (request, response) => {
-  response.send("Welcome to the API");
+  response.send('Welcome to the API');
 });
 
-//takes in any reply message
+// handle any message sent to Twilio ---
 app.post('/api/sms', (request, response) => {
   const twiml = new MessagingResponse();
   const body = request.body.Body;
 
-  twiml.message(`you just sent: ${body} to simple reservation app`);
+  twiml.message(`your message: ${body}`);
 
-  response.writeHead(200, {'Content-Type': 'text/xml'});
+  response.writeHead(200, { 'Content-Type': 'text/xml' });
   response.end(twiml.toString());
 });
 
-
-
 exports.api = functions.https.onRequest(app);
 
+/**********************
+ * Firestore Triggers
+ ***********************/
+exports.sendSMS = functions.firestore
+  .document('Reservations/{ReservatonId}')
+  .onCreate((change, context) => {
+    const client = require('twilio')(CONFIG.sid, CONFIG.token);
 
-/*******************
-* Other Functions
-********************/
+    client.messages.create({
+      body: 'Your Reservation has been made.',
+      to: CONFIG.testNumber,  // Text this number
+      from: CONFIG.twilioNumber // From a valid Twilio number
+    })
+      .then((message) => console.log(message.sid));
 
-// exports.smstest = functions.firestore.document('Reservation').onWrite((change, context) => {
-//   var client = new twilio(CONFIG.sid, CONFIG.token);
-
-//   client.messages.create({
-//     body: 'Hello from Node',
-//     to: 'number',  // Text this number
-//     from: 'number' // From a valid Twilio number
-//   })
-//     .then((message) => console.log(message.sid));
-
-// });
+  });
