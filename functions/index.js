d64d1e8c9f1935a52fbff833f640513813fbcc55
Ivan Villa <ivan@wearescribe.com>
2018-06-09 12:51:11 -0700

    converted time to 12hour
    the confirmation text now shows in 24 hour time

--- functions/index.js
+++ functions/index.js
@@ -19,10 +19,22 @@ app.get('/api', (request, response) => {
 
 // handle any message sent to Twilio ---
 app.post('/api/sms', (request, response) => {
-  const twiml = new MessagingResponse();
-  const body = request.body.Body;
+  var twiml = new MessagingResponse();
+  var body = request.body.Body.toLowerCase().trim();
+  var msg;
 
-  twiml.message(`your message: ${body}`);
+  switch (body) {
+    case 'confirm':
+      msg = "thanks for confirming";
+      break;
+    case 'can\'t go':
+      msg = "your reservation has been canceled";
+      break;
+    default:
+      msg = "sorry, please respond with CONFIRM or CAN'T GO";
+  }
+
+  twiml.message(msg);
 
   response.writeHead(200, { 'Content-Type': 'text/xml' });
   response.end(twiml.toString());
@@ -35,11 +47,29 @@ exports.api = functions.https.onRequest(app);
  ***********************/
 exports.sendSMS = functions.firestore
   .document('Reservations/{ReservatonId}')
-  .onCreate((change, context) => {
+  .onCreate((snap, context) => {
     const client = require('twilio')(CONFIG.sid, CONFIG.token);
+    var res = snap.data();
+
+    function convertTime(slot) {
+      var time = slot.split(":");
+      var hours = time[0];
+      var minutes = time[1];
+      var meredian = hours >= 12 ? "pm" : "am";
+
+      //convert to ampm
+      if (hours > 12) {
+        hours -= 12;
+      }
+      if (hours == 00) {
+        hours = 12;
+      }
+
+      return hours + ":" + minutes + meredian;
+    }
 
     client.messages.create({
-      body: 'Your Reservation has been made.',
+      body: `Your reservation with ${res.barberName} has been scheduled for ${convertTime(res.slot)}. Reply with CONFIRM or cancel at any time by replying CAN'T GO`,
       to: CONFIG.testNumber,  // Text this number
       from: CONFIG.twilioNumber // From a valid Twilio number
     })
