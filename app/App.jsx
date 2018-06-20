a1e79a8cdb0d6524c571f025420fb63770dc8c1a
Evghenii Ghimazitdinov <evgimov@gmail.com>
2018-06-20 08:32:05 -0700

    Added react-router module and routes

--- app/App.jsx
+++ app/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ReservationPage from './components/ReservationPage';
+import AdminDashboard from './components/AdminDashboard';
 import './server.js';
 
-import ReservationPage from './components/ReservationPage';
 
 //Main App
 const App = () => (
-  <ReservationPage />
+  <Router>
+    <div>
+      <Route exact path="/" component={ReservationPage} />
+      <Route path="/admin" component={AdminDashboard} />
+    </div>
+  </Router>
 );
 
 render(<App />, document.getElementById('app'));
