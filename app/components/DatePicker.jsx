6c8187f1ce2dabcd60ceb500ae867f3e1cfbd8fc
evgimov <evgimov@gmail.com>
2018-08-25 11:39:07 -0700

    Added presets and declaration of SingleDatePicker to the component

--- app/components/DatePicker.jsx
+++ app/components/DatePicker.jsx
@@ -4,30 +4,86 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-
-
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import moment from 'moment';
+import momentPropTypes from 'react-moment-proptypes';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
 	root: {
 		flexGrow: 1,
 	}
-});		
+});
+
+const defaultProps = {
+	autoFocus: false,
+	initialDate: null
+}
+
+const today = moment();
+const tomorrow = moment().add(1, 'day');
+const presets = [{
+	text: 'Today',
+	date: today
+},
+{
+	text: 'Tomorrow',
+	date: tomorrow
+},
+{
+	text: moment().add(2, 'days').format("D"),
+	date: moment().add(2, 'days')
+},
+{
+	text: moment().add(3, 'days').format("D"),
+	date: moment().add(3, 'days')
+},
+{
+	text: moment().add(4, 'days').format("D"),
+	date: moment().add(4, 'days')
+}
+
+];
 
 class DatePicker extends React.Component {
-  
-	constructor(props){
-		super(props);
-		this.state = { 
 
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: props.initialDate,
+			focused: props.autoFocus
+		};
+		this.onDateChange = this.onDateChange.bind(this);
+		this.onFocusChange = this.onFocusChange.bind(this);
+	}
+
+	onDateChange(date) {
+		this.setState({ date });
+	}
+
+	onFocusChange(focused) {
+		this.setState({ focused });
 	}
 
 	render() {
 		const { classes } = this.props;
-		return (  
+		return (
 			<div className={classes.root}>
-				<Grid container spacing={24}>
-          <h1>Hello from DatePicker component</h1>
+				<Grid container spacing={24} direction='row'>
+					<Grid item xs={12}>
+						{presets.map(({ text, date }) => {
+							return <Button onClick={() => this.onDateChange(date)}>{text}</Button>
+						})}
+						<SingleDatePicker
+							date={this.state.date} // momentPropTypes.momentObj or null
+							onDateChange={this.onDateChange} // PropTypes.func.isRequired
+							focused={this.state.focused} // PropTypes.bool
+							onFocusChange={this.onFocusChange} // PropTypes.func.isRequired
+							id="datepicker" // PropTypes.string.isRequired,
+						/>
+					</Grid>
 				</Grid>
 			</div>
 		);
@@ -35,7 +91,10 @@ class DatePicker extends React.Component {
 }
 
 DatePicker.propTypes = {
-  classes: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
+	onDateChange: PropTypes.func.isRequired,
+	onFocusChange: PropTypes.func.isRequired,
+	initialDate: momentPropTypes.momentObj
 };
 
 export default withStyles(styles)(DatePicker);
@@ -44,5 +103,4 @@ export default withStyles(styles)(DatePicker);
 
 
 
-      
-      
+
