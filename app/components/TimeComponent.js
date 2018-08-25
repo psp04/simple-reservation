c582c9cac328abc5c690f0fa569b12624001a232
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-08-25 15:03:36 -0700

    Added style and initial component file

--- app/components/TimeComponent.js
+++ app/components/TimeComponent.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Slider from "react-slick";
 
-class TimeComponent extends React.Component {
+class TimeComponent extends React.Component {    
     render() {
+        const arrayList = [500,530,600,700,800,900];
+
+        const settings = {
+            //className: "center",
+            infinite: false,
+            //centerPadding: "2px",
+            slidesToShow: 5,
+            swipeToSlide: true
+        };
+
         return (
             <div>
-                hello!
-            </div>
-        )
+                <h2>Time Component</h2>
+                <Slider {...settings}>
+                    {arrayList.map((element) => (
+                        <div className="pill" key={element}>{element}</div>
+                    ))}
+                </Slider> 
+            </div> 
+        );
     }
 }
 
