d97105350eabcbeba69f0bfb60e45dff6e34e1d8
Michelle Lim <mlim@riskalyze.com>
2018-08-25 15:07:33 -0700

    Formatting

--- app/components/ServicePicker.jsx
+++ app/components/ServicePicker.jsx
@@ -6,45 +6,37 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import SimpleSlider from './SimpleSlider';
 
-
 const styles = theme => ({
-	root: {
-		flexGrow: 1,
-	}
-});		
+    root: {
+        flexGrow: 1,
+    }
+});
 
 class ServicePicker extends React.Component {
-  
-	constructor(props){
-		super(props);
-		this.state = { 
 
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
 
-	render() {
-		const { classes } = this.props;
-		return (  
-			<div className={classes.root}>
-				<Grid container spacing={24}>
+        }
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <Grid container spacing={24}>
                     <h1>Hello from ServicePicker component</h1>
                     <SimpleSlider>Test</SimpleSlider>
-				</Grid>
-                
-			</div>
-		);
-	}
+                </Grid>
+
+            </div>
+        );
+    }
 }
 
 ServicePicker.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(ServicePicker);
-
-
-
-
-
-      
-      
