61a3f4b80ebddec796e16b5cb35f07738b91bf77
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-08-25 18:39:25 -0700

    Finished the tiem component

--- app/components/TimeComponent.js
+++ app/components/TimeComponent.js
@@ -1,7 +1,25 @@
 import React from "react";
 import Slider from "react-slick";
 
-class TimeComponent extends React.Component {    
+class TimeComponent extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            selectedTime: undefined
+        }
+
+        this.handleSelected = (event) => {
+            const selectedTime = parseInt(event.target.id);
+            //console.log(event.target.className);
+
+            this.setState(() => ({ selectedTime }))
+            document.querySelectorAll(".pill").forEach((element) => {
+                document.getElementById(element.id).classList.remove("pill-active");
+            });
+            document.getElementById(event.target.id).classList.add("pill-active"); 
+        }
+    }
+
     render() {
         const arrayList = [500,530,600,700,800,900];
 
@@ -9,7 +27,7 @@ class TimeComponent extends React.Component {
             //className: "center",
             infinite: false,
             //centerPadding: "2px",
-            slidesToShow: 5,
+            slidesToShow: 3,
             swipeToSlide: true
         };
 
@@ -18,9 +36,15 @@ class TimeComponent extends React.Component {
                 <h2>Time Component</h2>
                 <Slider {...settings}>
                     {arrayList.map((element) => (
-                        <div key={element}>{element}</div>
+                        <div key={element}>
+                            <button className="pill" 
+                                    id={element}
+                                    onClick={this.handleSelected}
+                            >{element}</button>
+                        </div>
                     ))}
                 </Slider> 
+                <h4>This is the current State: {this.state.selectedTime}</h4>
             </div> 
         );
     }
