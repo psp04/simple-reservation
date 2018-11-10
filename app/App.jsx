3a9cb66284c8d6123496540f41fa8f68f0bd69e9
evgimov <evgimov@gmail.com>
2018-11-10 11:50:12 -0800

    Created new layout for the reservation page component and added material ui to the project

--- app/App.jsx
+++ app/App.jsx
@@ -3,11 +3,8 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ReservationPage from './components/ReservationPage';
 import AdminDashboard from './components/AdminDashboard';
-<<<<<<< HEAD
 import TimeComponent from './components/TimeComponent';
-=======
 import DatePicker from './components/DatePicker';
->>>>>>> origin/master
 import './server.js';
 import './App.css';
 
@@ -18,11 +15,8 @@ const App = () => (
     <div>
       <Route exact path="/" component={ReservationPage} />
       <Route path="/admin" component={AdminDashboard} />
-<<<<<<< HEAD
       <Route path="/time" component={TimeComponent} />
-=======
       <Route path="/datepicker" component={DatePicker} />
->>>>>>> origin/master
     </div>
   </Router>
 );
