f8a60ad9c04cd6b8db8336b337b4107f51c7dfb5
evgimov <evgimov@gmail.com>
2018-11-10 10:46:38 -0800

    Refactoring of the time component

--- app/components/TimeComponent.js
+++ app/components/TimeComponent.js
@@ -1,53 +1,60 @@
 import React from "react";
 import Slider from "react-slick";
 
+
+const arrayList = [500, 530, 600, 700, 800, 900]; 
+
+const settings = {
+  //className: "center",
+  infinite: false,
+  //centerPadding: "2px",
+  slidesToShow: 3,
+  swipeToSlide: true
+};
+
 class TimeComponent extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { 
-            selectedTime: undefined
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTime: 0
+    };
+    this.handleSelected = this.handleSelected.bind(this);
+  }
 
-        this.handleSelected = (event) => {
-            const selectedTime = parseInt(event.target.id);
-            //console.log(event.target.className);
+  handleSelected(event){
+    this.setState({[event.target.name]: event.target.id }); 
 
-            this.setState(() => ({ selectedTime }))
-            document.querySelectorAll(".pill").forEach((element) => {
-                document.getElementById(element.id).classList.remove("pill-active");
-            });
-            document.getElementById(event.target.id).classList.add("pill-active"); 
-        }
-    }
+    document.querySelectorAll(".pill").forEach(element => {
+      if (element.id === event.target.id){
+        document.getElementById(event.target.id).classList.add("pill-active");
+      } else {
+        document.getElementById(element.id).classList.remove("pill-active");
+      }      
+    });
+    
+  }
 
-    render() {
-        const arrayList = [500,530,600,700,800,900];
-
-        const settings = {
-            //className: "center",
-            infinite: false,
-            //centerPadding: "2px",
-            slidesToShow: 3,
-            swipeToSlide: true
-        };
-
-        return (
-            <div>
-                <h2>Time Component</h2>
-                <Slider {...settings}>
-                    {arrayList.map((element) => (
-                        <div key={element}>
-                            <button className="pill" 
-                                    id={element}
-                                    onClick={this.handleSelected}
-                            >{element}</button>
-                        </div>
-                    ))}
-                </Slider> 
-                <h4>This is the current State: {this.state.selectedTime}</h4>
-            </div> 
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Time Component</h2>
+        <Slider {...settings}>
+          {arrayList.map(element => (
+            <div key={element}>
+              <button
+                className="pill"
+                id={element}
+                name="selectedTime"
+                onClick={this.handleSelected}
+              >
+                {element}
+              </button>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
 }
 
 export default TimeComponent;
