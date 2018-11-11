ea44909afbecb82cccf06a9018e45ce8d38ec9b4
evgimov <evgimov@gmail.com>
2018-11-11 15:54:34 -0800

    Added layout for displaying the services and added styling to the component

--- app/components/ServicePicker.jsx
+++ app/components/ServicePicker.jsx
@@ -1,6 +1,4 @@
 /* Todo
- * loop through the services and check if some div was selected
- * if some div was selected then update the current image source
  * keep selectedService type as a state in the ReservationPage component
  * import and render the ServicePicker component inside the ReservationPage component
  * refactor the barbers dropdown component
@@ -8,8 +6,14 @@
  * connect servicepicker component and barber component to update dropdown selection options depending on the selectedService type
  */
 
+/*Notes
+ * research possibility to get all images from firestore storage
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import Slider from "react-slick";
 import firebase from "@firebase/app";
 import firestore from "../server.js";
@@ -20,17 +24,38 @@ const settings = {
   swipeToSlide: true
 };
 
+const styles = {
+  service: {
+    textAlign: "center",
+    width: "50px",
+    height: "80px"
+  },
+
+  serviceImage: {
+    width: "50px"
+  }
+};
+
+function arraymove(arr, fromIndex, toIndex) {
+  var element = arr[fromIndex];
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+}
+
 class ServicePicker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedService: undefined,
+      selectedServiceName: "-",
       servicesArray: []
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     var tempArray = [];
+    var middlePositionIndex = 0;
+    var currentPosition = 0;
     firestore
       .collection("Services")
       .get()
@@ -40,21 +65,90 @@ class ServicePicker extends React.Component {
           service.id = doc.id;
           tempArray.push(service);
         });
+        if (tempArray.length%2 === 0){
+          middlePositionIndex = tempArray.length / 2 - 1;
+        } else{
+          middlePositionIndex = Math.round(tempArray.length / 2) - 1;
+        }
+        console.log(middlePositionIndex);
+
+        for (var i=0; i < tempArray.length; i++){
+          if (tempArray[i].name === "-"){
+            currentPosition = i; break;
+          }
+        }
+        arraymove(tempArray,currentPosition,middlePositionIndex);
+        console.log(tempArray);
         this.setState({ servicesArray: tempArray });
       });
   }
 
+  handleClick(event) {
+    console.log(event.target.id);
+    const { id } = event.target;
+    this.setState({ selectedServiceName: id });
+  }
+
   render() {
-    const { servicesArray } = this.state;
+    const { servicesArray, selectedServiceName } = this.state;
+    const { classes } = this.props;
+    console.log(servicesArray);
+    
 
     return (
       <div>
-        {servicesArray.map(item => {
-          console.log(item);
-        })}
+        <Slider {...settings}>
+          {servicesArray.map(service =>
+            service.name === "-" ? (
+              <div
+                key={service.id}
+                className={classes.service}
+                onClick={this.handleClick}
+              >
+                <img
+                  className={classes.serviceImage}
+                  id={service.name}
+                  src={service.imgUrl}
+                  alt={service.name}
+                />
+                <span style={{ color: "#364488" }}>{service.name}</span>
+              </div>
+            ) : (
+              <div
+                key={service.id}
+                className={classes.service}
+                onClick={this.handleClick}
+              >
+                <img
+                  className={classes.serviceImage}
+                  id={service.name}
+                  src={
+                    selectedServiceName === service.name
+                      ? service.imgSelectedUrl
+                      : service.imgUrl
+                  }
+                  alt={service.name}
+                />
+                <span
+                  style={
+                    selectedServiceName === service.name
+                      ? { color: "#D62964" }
+                      : { color: "#979797" }
+                  }
+                >
+                  {service.name}
+                </span>
+              </div>
+            )
+          )}
+        </Slider>
       </div>
     );
   }
 }
 
-export default ServicePicker;
+ServicePicker.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(ServicePicker);
