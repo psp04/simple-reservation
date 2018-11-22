83f55e1bbe5bd7fd101d5842dc5062f133f2c4d5
evgimov <evgimov@gmail.com>
2018-11-22 12:19:01 -0800

    Transformed ReservationPage from func stateless component to the class declaration and added ServicePicker and BarbersDropdownList components to it

--- app/components/ReservationPage.jsx
+++ app/components/ReservationPage.jsx
@@ -1,54 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import TimeComponent from "./TimeComponent";
 import ServicePicker from "./ServicePicker";
 import BarbersDropdownList from "./BarbersDropdownList";
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   paper: {
     padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+    textAlign: "center",
+    color: theme.palette.text.secondary
   }
-  
 });
 
-const ReservationPage = (props) => {
+class ReservationPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedServiceName: ""
+    }
+    this.getSelectedService = this.getSelectedService.bind(this);
+  }
 
-  const {classes} = props;
+  getSelectedService(name) {
+    this.setState({ selectedServiceName: name });
+  }
 
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={24}>
-        <Grid item xs>
+  render() {
+    const { classes } = this.props;
+    const { selectedServiceName } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs />
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+              <ServicePicker getSelectedService={this.getSelectedService} />
+            </Paper>
+            <Paper className={classes.paper}>
+              <BarbersDropdownList selectedServiceName={selectedServiceName}/>
+            </Paper>
+            <Paper className={classes.paper}>
+              <TimeComponent />
+            </Paper>
+          </Grid>
+          <Grid item xs />
         </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>
-            <ServicePicker />
-          </Paper>
-          <Paper className={classes.paper}>
-            <BarbersDropdownList />
-          </Paper>
-          <Paper className={classes.paper}>
-            <TimeComponent />
-          </Paper>
-        </Grid>
-        <Grid item xs>
-          
-        </Grid>
-      </Grid>
-    </div>      
-  );
-};
+      </div>
+    );
+  }
+}
 
 ReservationPage.propTypes = {
   classes: PropTypes.object.isRequired
-}
+};
 
 export default withStyles(styles)(ReservationPage);
