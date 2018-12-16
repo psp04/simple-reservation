01dfea36845911233e8de98b47e57f4e80f9849e
evgimov <evgimov@gmail.com>
2018-12-15 23:18:07 -0800

    Overrode the styles for Select and OutlinedInput components by using classes property

--- app/components/BarbersDropdownList.js
+++ app/components/BarbersDropdownList.js
@@ -1,17 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import firebase from "@firebase/app";
 import firestore from "../server.js";
 
+
 const styles = theme => ({
-  selectList: {
-    marginLeft: "25%",
-    marginRight: "25%",
-    width: "50%"
+  outlinedInputRoot: {
+    marginLeft: "30%",
+    marginRight: "30%",
+    width: "40%"
+  },
+  notchedOutlineInput: {
+    borderColor: "#364488 !important",
+    borderRadius: "8px"
+  },
+  selectMenu: {
+    color: "#364488",
+    borderColor: "#364488",
+    padding: "10px 0px 10px 15px"
+  },
+  selectMenuSelected: {
+    color: "#d62964",
+    borderColor: "#364488",
+    padding: "10px 0px 10px 15px",
+    fontWeight: "500"
+  },
+  
+  icon: {
+    top: "calc(50% - 20px)",
+    color: "#364488",
+    fontSize: "40px"
+  },
+  iconSelected: {
+    top: "calc(50% - 20px)",
+    color: "#d62964",
+    fontSize: "40px"
   }
 });
 
@@ -63,9 +91,24 @@ class BarbersDropdownList extends React.Component {
       <Select
         value={selectedBarber}
         onChange={this.handleChange}
+        variant="outlined"
+        input={              
+          <OutlinedInput
+            name="selectedBarber"
+            id="selectedBarberId"
+            classes={{
+              root: classes.outlinedInputRoot,
+              notchedOutline: classes.notchedOutlineInput
+            }}
+          />
+        }
         displayEmpty
         name="selectedBarber"
-        className={classes.selectList}
+        
+        classes={{
+          selectMenu: selectedBarber === "" ? classes.selectMenu : classes.selectMenuSelected,
+          icon: selectedBarber === "" ? classes.icon : classes.iconSelected
+        }}
       >
         {barberList.map(barber => {
           return <MenuItem key={barber.id}>{barber.name}</MenuItem>;
